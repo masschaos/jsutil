090dd937f6925550c054f508c47af9a1b7b85787
Muninn <hyacinthus@gmail.com>
2020-07-05 22:37:34 +0800

    npm publish

--- packages/im/test.js
+++ packages/im/test.js
@@ -1,6 +1,6 @@
 const im = require('./')
 
-im.setToken('')
+im.setToken(process.env.SLACK_TOKEN)
 im.setChannels('dev', 'dev', 'dev')
 const context = {
   1: '这是Test1',
@@ -13,6 +13,10 @@ context1.set('123', 'ssss')
 context1.set('123456', [1, 2, 3])
 
 im.debug('debug !!!', context).then(res => {
+  console.log(res)
+})
+
+im.info('*INFO*\nSomething you want to know.\n1. hello\n2. world', context).then(res => {
   console.log(res)
 })
 
